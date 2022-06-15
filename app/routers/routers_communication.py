514bc3902495247042c07f7158fca53864626374
Mikailo <m.kontoulis.cs@gmail.com>
2022-06-15 18:09:59 +0300

    -Added existing function router -untested

--- app/routers/routers_communication.py
+++ app/routers/routers_communication.py
@@ -13,6 +13,17 @@ TestRunId = os.environ.get('TestRunId') if os.environ.get(
 TestStepId = os.environ.get('TestStepId') if os.environ.get(
     'WFAddress') else "db4a0c7d-0e09-49b2-ba80-7c5e4e3f0768"
 
+ExistingFunctions = [
+    "auto-correlation",
+    "partial-auto-correlation",
+    "filters",
+    "welch",
+    "find-peaks",
+    "periodogram",
+    "spindle",
+    "recon-all",
+    "samseg"
+]
 @router.get("/test/task/ping", tags=["test_task_ping"])
 async def test_task_ping() -> dict:
     # channels = data.ch_names
@@ -43,8 +54,25 @@ async def test_task_complete() -> dict:
     return {'test': "test"}
 
 
-@router.post("/task/handling", tags=["task_handling"], status_code=200)
-async def task_handling(request: Request) -> dict:
+@router.post("/function/handling", tags=["function_handling"], status_code=200)
+async def function_handling(request: Request) -> dict:
     # channels = data.ch_names
+    print(request.json())
+    return
+
+@router.post("/function/existing", tags=["function_existing"], status_code=200)
+async def task_existing(request: Request) -> dict:
+    # channels = data.ch_names
+    print(WFAddress)
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "functions": ExistingFunctions,
+    }
+    url = WFAddress + "/existing/functions" #Need to discuss this with EVOL
+    print(url)
+    response = requests.put(url=url, data=data, headers=headers)
+    print("Test Response: Task Existing Sent INformation")
+    print(response)
+
     print(request.json())
     return
