e31dfb1e650ef2c17aa26124afc094ce96c288b8
Mikailo <m.kontoulis.cs@gmail.com>
2022-09-28 15:39:45 +0000

    New production config

--- app/main.py
+++ app/main.py
@@ -164,12 +164,7 @@ app = FastAPI(openapi_tags=tags_metadata)
 # region CORS Setup
 # This region enables FastAPI's built in CORSMiddleware allowing cross-origin requests allowing communication with
 # the React front end
-origins = [
-    "http://localhost:3000",
-    "http://localhost:3000/auto_correlation",
-    "localhost:3000"
-    "localhost:3000/auto_correlation"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
