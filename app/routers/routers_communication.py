d1d6c86f2d00d085083e4c7dfbadd7ee8fe35324
Michael <m.kontoulis.cs@gmail.com>
2024-01-18 17:13:16 +0200

    Updated save location of save data

--- app/routers/routers_communication.py
+++ app/routers/routers_communication.py
@@ -516,7 +516,7 @@ async def function_save_data(
             files_to_upload = [f for f in os.listdir(path_to_storage + '/output') if isfile(join(path_to_storage + '/output', f))]
             for file in files_to_upload:
                 out_filename = path_to_storage + '/output/' + file
-                upload_object(bucket_name="common", object_name='expertsystem/workflow/'+ workflow_id+'/'+ run_id+'/'+
+                upload_object(bucket_name="common", object_name='workflows/'+ workflow_id+'/'+ run_id+'/'+
                                                               step_id+'/analysis_output/' + file, file=out_filename)
             return JSONResponse(content='info.json file has been successfully uploaded to the DataLake', status_code=200)
         except Exception as e:
@@ -529,7 +529,7 @@ async def function_save_data(
             output_filename = os.path.join(tmpdir, 'ucl_test')
             print(output_filename)
             print(shutil.make_archive(output_filename, 'zip', root_dir=path_to_storage, base_dir='output/ucl_test'))
-            upload_object(bucket_name="common", object_name='expertsystem/workflow/' + workflow_id + '/' + run_id + '/' +
+            upload_object(bucket_name="common", object_name='workflows/' + workflow_id + '/' + run_id + '/' +
                                                            step_id + '/output/ucl_test.zip',
                           file=output_filename + '.zip')
 
