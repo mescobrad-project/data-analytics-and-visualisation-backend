52aed1511124dd86efbbc98a93a29d58e0b199fc
Michael <m.kontoulis.cs@gmail.com>
2024-01-18 17:18:19 +0200

    Updated save location of save data

--- app/routers/routers_communication.py
+++ app/routers/routers_communication.py
@@ -517,7 +517,7 @@ async def function_save_data(
             for file in files_to_upload:
                 out_filename = path_to_storage + '/output/' + file
                 upload_object(bucket_name="common", object_name='workflows/'+ workflow_id+'/'+ run_id+'/'+
-                                                              step_id+'/analysis_output/' + file, file=out_filename)
+                                                              step_id + '/' + file, file=out_filename)
             return JSONResponse(content='info.json file has been successfully uploaded to the DataLake', status_code=200)
         except Exception as e:
             print(e)
@@ -530,7 +530,7 @@ async def function_save_data(
             print(output_filename)
             print(shutil.make_archive(output_filename, 'zip', root_dir=path_to_storage, base_dir='output/ucl_test'))
             upload_object(bucket_name="common", object_name='workflows/' + workflow_id + '/' + run_id + '/' +
-                                                           step_id + '/output/ucl_test.zip',
+                                                           step_id + '/ucl_test.zip',
                           file=output_filename + '.zip')
 
             return JSONResponse(content='zip file has been successfully uploaded to the DataLake', status_code=200)
