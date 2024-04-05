44d472cfc204471f666ba2452b6033fde9c3e6c3
Mikailo <m.kontoulis.cs@gmail.com>
2024-04-05 14:19:06 +0300

    -ai changes

--- app/utils/training.py
+++ app/utils/training.py
@@ -75,7 +75,7 @@ def train_eval_model(train_dataloader, eval_dataloader, model, lr, patience):
         dev_f1 = metrics.f1_score(eval_targets, eval_predictions, zero_division=1)
         dev_acc = metrics.accuracy_score(eval_targets, eval_predictions)
 
-        print(f'epoch {epoch+1} - train loss {train_loss:.3f} - val loss {valid_loss:.3f} - val f1 {dev_f1:.3f} - val acc {dev_acc:.3f}')
+        print(f'epoch {epoch+1} - train loss {train_loss:.3f} - val loss {valid_loss:.3f} - val f1 {dev_f1:.3f} - val acc {dev_acc:.3f}', flush=True)
 
         # Early Stopping
         if round(valid_loss, 3) < threshold: #improvement
@@ -87,14 +87,14 @@ def train_eval_model(train_dataloader, eval_dataloader, model, lr, patience):
         
         if patience_counter == patience:
             es_epoch = max(epoch + 1 - patience, 1)
-            print(f'Early Stopping checkpoint at epoch {es_epoch}. Patience value was {patience}.')
-            print('Train-Eval stage complete!')
+            print(f'Early Stopping checkpoint at epoch {es_epoch}. Patience value was {patience}.', flush=True)
+            print('Train-Eval stage complete!', flush=True)
             
             for epoch in range(es_epoch):
                 # Train the best_model on the validation data as well
                 optimizer = torch.optim.Adam(best_model.parameters(), lr=lr) # leave lr value as the original one?
                 train_model(eval_dataloader, best_model, optimizer)
-            print('Training on eval data complete!')
+            print('Training on eval data complete!', flush=True)
                 
             break
 
