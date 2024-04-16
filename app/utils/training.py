859b76cfa09d9d11e4ae6034eb3cc71e5c549a4c
Vangelis <vlamprou@epu.ntua.gr>
2024-04-16 14:04:26 +0300

    scheduler.step(valid_loss) due to Plateu

--- app/utils/training.py
+++ app/utils/training.py
@@ -79,7 +79,7 @@ def train_eval_model(train_dataloader,
 
         # Validation phase
         valid_loss, eval_targets, eval_predictions = evaluate_model(eval_dataloader, model)
-        scheduler.step()
+        scheduler.step(valid_loss)
         dev_f1 = metrics.f1_score(eval_targets, eval_predictions, zero_division=1)
         dev_acc = metrics.accuracy_score(eval_targets, eval_predictions)
 
