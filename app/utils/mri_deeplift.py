56ef2a1a0ea3a9f7f07ebd37950941c3b20c3e5b
Vangelis <vlamprou@epu.ntua.gr>
2024-04-19 10:14:56 +0300

    memory test for mri_deeplift

--- app/utils/mri_deeplift.py
+++ app/utils/mri_deeplift.py
@@ -28,34 +28,60 @@ def visualize_dl(model_path,
     assert (os.path.exists(mri_path))
     assert (os.path.exists(heatmap_path))
 
+    #--load model
     model = torch.load(model_path)
+    wrapped_model = Conv3DWrapper(model)
 
+    #--load mri
     nii_img = nib.load(mri_path) #3-dim mri
     mri = nii_img.get_fdata()
     tensor_mri = torch.from_numpy(mri)
     tensor_mri = torch.unsqueeze(tensor_mri, 0)
     tensor_mri = torch.unsqueeze(tensor_mri, 0) #5-dim torch Tensor [1,1,160,256,256] (verified)
 
+    print(model_path,
+                 mri_path,
+                 heatmap_path,
+                 heatmap_name,
+                 slice,
+                 alpha)
+
+    return model_path,
+                 mri_path,
+                 heatmap_path,
+                 heatmap_name,
+                 slice,
+                 alpha
+
+    '''
+    #--send to device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('device', device)
     tensor_mri = tensor_mri.to(device)
     print('tensor_mri', tensor_mri.shape)
     #model = model.to(device)
-
-    wrapped_model = Conv3DWrapper(model)
     wrapped_model.to(device)
     print(wrapped_model)
 
+    #--deeplift
     dl = DeepLift(wrapped_model)
-
     output = model(tensor_mri)
     target_class = int(torch.argmax(output[1])) #int: this should be int 0 or 1 (verified)
     print('target class', target_class)
 
-    attributions = dl.attribute(tensor_mri,
-                                target=target_class)
-    print('attributions calculated! shape is', attributions.shape) #5-dim torch Tensor [1,1,160,256,256] (verified)
+    # Track GPU memory usage
+    initial_memory = torch.cuda.memory_allocated(device)
+    print('Initial GPU Memory Allocated:', initial_memory)
 
+    attributions = dl.attribute(tensor_mri, target=target_class)
+
+    final_memory = torch.cuda.memory_allocated(device)
+    max_memory = torch.cuda.max_memory_allocated(device)
+    print('Attributions calculated! Shape:', attributions.shape)
+    print('Final GPU Memory Allocated:', final_memory)
+    print('Max GPU Memory Allocated:', max_memory)
+
+    #--plot
     heatmap = attributions.cpu().squeeze().squeeze().permute(1, 2, 0).numpy() #[256, 256, 160] numpy array (verified)
     print('heatmap calculated! shape is', heatmap.shape)
 
