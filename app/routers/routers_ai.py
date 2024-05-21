a64ff16c6b753f261f61133bbc1f2a642a77b08c
vlamprou <vlamprou@epu.ntua.gr>
2024-05-21 13:54:37 +0300

    .

--- app/routers/routers_ai.py
+++ app/routers/routers_ai.py
@@ -16,12 +16,10 @@ async def ai_mri_training_experiment(
         iterations : int = 5,
         es_patience: int = 3
        ) -> dict:
-
     results = run_experiment(data_path,
-                   csv_path,
-                   iterations,
-                   es_patience
-                   )
+                             csv_path,
+                             iterations,
+                             es_patience)
     return {"results": results}
     # files = get_files_for_slowwaves_spindle(workflow_id, run_id, step_id)
     # path_to_storage = get_local_storage_path(workflow_id, run_id, step_id)
