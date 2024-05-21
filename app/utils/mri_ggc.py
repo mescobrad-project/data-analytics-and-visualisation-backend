d285bc10bc7d7fd4272ba6cfc9b39e624826907a
vlamprou <vlamprou@epu.ntua.gr>
2024-05-21 13:23:25 +0300

    top_prob[0] for plot

--- app/utils/mri_ggc.py
+++ app/utils/mri_ggc.py
@@ -115,7 +115,7 @@ def visualize_ggc(model_path,
                                                          N=5000),
                   interpolation='gaussian')
 
-    ax.set_title('MRI(Grey) vs DeepLift Attributions(Blue) Overlay\n' + f'pred: {group} (prob: {top_prob})\n' + f'{axis} slice {slice}')
+    ax.set_title('MRI(Grey) vs DeepLift Attributions(Blue) Overlay\n' + f'pred: {group} (prob: {top_prob[0]})\n' + f'{axis} slice {slice}')
 
     # Save and show the plot
     plt.savefig(os.path.join(heatmap_path, heatmap_name))
