80920bc0e63df1eb9a1a6a1804643c57e40657f4
Mikailo <m.kontoulis.cs@gmail.com>
2024-05-24 13:32:22 +0300

    -Fix token authentication, -Added Origins specifically and SessionMiddleware -Updated Minio upload and download data

--- app/main.py
+++ app/main.py
@@ -184,7 +184,12 @@ app = FastAPI(openapi_tags=tags_metadata)
 # region CORS Setup
 # This region enables FastAPI's built in CORSMiddleware allowing cross-origin requests allowing communication with
 # the React front end
-origins = ["*"]
+origins = [
+           "http://localhost:8000",
+           "http://localhost:3005",
+            "https://analytics.platform.mes-cobrad.eu",
+            "https://analyticsbk.platform.mes-cobrad.eu"
+          ]
 
 app.add_middleware(
     CORSMiddleware,
@@ -195,7 +200,7 @@ app.add_middleware(
 
 )
 
-app.add_middleware(SessionMiddleware, secret_key="random-string")
+app.add_middleware(SessionMiddleware, secret_key="random-string",  same_site="None")
 
 NeurodesktopStorageLocation = os.environ.get('NeurodesktopStorageLocation') if os.environ.get(
     'NeurodesktopStorageLocation') else "/neurodesktop-storage"
