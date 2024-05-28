5160f86f6c67cdae14897f07df6b91420ba9a9ef
vlamprou <vlamprou@epu.ntua.gr>
2024-05-28 23:04:35 +0300

    removed saving the attributions

--- app/utils/mri_deeplift.py
+++ app/utils/mri_deeplift.py
@@ -58,7 +58,7 @@ def visualize_dl(model_path,
     dl = DeepLift(wrapped_model)
     attributions = dl.attribute(tensor_mri, target=top_class.item()).detach().cpu().squeeze().permute(1, 2, 0).numpy() #(256, 256, 160)
     #nib.save(nib.Nifti1Image(attributions, affine=np.eye(4)), os.path.join(heatmap_path, 'attributions.nii'))
-    np.save(os.path.join(heatmap_path, 'attributions.npy'), attributions)
+    #np.save(os.path.join(heatmap_path, 'attributions.npy'), attributions)
 
     torch.cuda.empty_cache()
 
