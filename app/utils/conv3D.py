f07cd81a8a6ca74a35e655c7a924348e3f85ac4e
vlamprou <vlamprou@epu.ntua.gr>
2024-05-29 16:15:29 +0300

    removed group6 from conv3d

--- app/utils/conv3D.py
+++ app/utils/conv3D.py
@@ -30,7 +30,7 @@ class Conv3D(nn.Module):
             nn.Conv3d(256, 512, kernel_size=3, padding=1),
             nn.BatchNorm3d(512),
             nn.ReLU(),
-            nn.Dropout(0.15),
+            nn.Dropout(0.2),
             nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)))
         self.group5 = nn.Sequential(
             nn.Conv3d(512, 512, kernel_size=3, padding=1),
@@ -52,7 +52,7 @@ class Conv3D(nn.Module):
         out = self.group3(out)
         out = self.group4(out)
         out = self.group5(out)
-        out = self.group6(out)
+        #out = self.group6(out)
         y = torch.mean(out.view(out.size(0), out.size(1), -1), dim=2)
         logits = self.classifier(y)
 
