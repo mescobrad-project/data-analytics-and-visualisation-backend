71f3f558e1b2eca8a1ae2b4c7b0a7621b51a3d60
vlamprou <vlamprou@epu.ntua.gr>
2024-05-29 20:24:16 +0300

    unfreeze the last layer during training

--- app/utils/resnet18_3d.py
+++ app/utils/resnet18_3d.py
@@ -4,13 +4,13 @@ from collections import OrderedDict
 
 class ResNet18_3D(nn.Module):
 
-    def __init__(self,
-                 trainable_feature_layers):
+    def __init__(self):
         super(ResNet18_3D, self).__init__()
 
-        resnet = torchvision.models.video.r3d_18(pretrained=True)
-        resnet.fc = nn.Linear(resnet.fc.in_features, 2)
+        self.resnet = torchvision.models.video.r3d_18(pretrained=True)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 2)
 
+        '''
         layers = [
             ('stem', resnet.stem),
             ('layer1_0', resnet.layer1[0]),
@@ -26,7 +26,7 @@ class ResNet18_3D(nn.Module):
         ]
 
         self.model = nn.Sequential(OrderedDict(layers))
-
+        
         # LAYERS TO FREEZE DURING TRAINING
         # 'adpt_avgpool' does not contain learnable params
         all_layers = [layer for name, layer in layers if name not in ['adpt_avgpool', 'clf']]
@@ -37,16 +37,21 @@ class ResNet18_3D(nn.Module):
             assert all(x in range(len(all_layers)) for x in
                        trainable_feature_layers), "Invalid layer indices in trainable_feature_layers"
             self.freeze = [layer for i, layer in enumerate(all_layers) if i not in trainable_feature_layers]
+        '''
 
-        for child in self.freeze:
+        layers_to_freeze = [resnet.layer1[0], resnet.layer1[1],
+                            resnet.layer2[0], resnet.layer2[1],
+                            resnet.layer3[0], resnet.layer3[1],
+                            resnet.layer4[0]]
+        for child in layers_to_freeze:
             for param in child.parameters():
                 param.requires_grad = False
 
     def trainable_params(self):
-        print('No. of trainable params', sum(p.numel() for p in self.model.parameters() if p.requires_grad))
+        print('No. of trainable params', sum(p.numel() for p in self.resnet.parameters() if p.requires_grad))
 
     def forward(self, x, labels=None):
-        outputs = self.model(x)
+        outputs = self.resnet(x)
 
         loss = None
         if labels is not None:
