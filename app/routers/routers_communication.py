139b2b278e67a11697fac6868dfa9ca7564b5806
Mikailo <m.kontoulis.cs@gmail.com>
2024-06-03 13:57:52 +0300

    temp change token

--- app/routers/routers_communication.py
+++ app/routers/routers_communication.py
@@ -183,6 +183,7 @@ class FunctionNavigationItem(BaseModel):
     run_id: str
     step_id: str
     function: str
+    token: str
     metadata: dict
 
 
@@ -547,19 +548,24 @@ async def function_navigation(navigation_item: FunctionNavigationItem, request: 
         # Handle files metadata missing from request/accept it as an empty array
         # print("TOKEN IS: ", request.session.get("my_token", None))
         print("TOKEN IS: ", request.session.get("secret_key"))
+        my_session_token = request.session.get("secret_key")
+        if my_session_token is None:
+            my_session_token = navigation_item.token
         if "files" in navigation_item.metadata:
             # print("KEY EXISTS")
             # print(navigation_item.metadata)
             create_local_step(workflow_id=navigation_item.workflow_id, run_id=navigation_item.run_id,
                               step_id=navigation_item.step_id, files_to_download=navigation_item.metadata["files"],
-                              session_token=request.session.get("secret_key"))
+                              # session_token=request.session.get("secret_key"))
+                              session_token=my_session_token)
                               # session_token="eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0MUVRLS11SElzcFlkanhIMU9wbWI5QUVSZ05fdlhfTE1BUGNwdGJPeTJFIn0.eyJleHAiOjE3MTY0NTc0NjQsImlhdCI6MTcxNjQ1NzE2NCwiYXV0aF90aW1lIjoxNzE2NDU3MTYzLCJqdGkiOiI2YmQ3OTFlNS04YTZmLTRhOTgtYWQ1Ny01MTYwYzQ5YTQ0YmIiLCJpc3MiOiJodHRwczovL2lkbS5kaWdpdGFsLWVuYWJsZXIuZW5nLml0L2F1dGgvcmVhbG1zL21lc2NvYnJhZCIsImF1ZCI6WyJob21lLWFwcCIsImFjY291bnQiXSwic3ViIjoiZjAyZjExYmItZDc4NC00NDdjLTlmYTEtZjliYmUxODg5MjlkIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiZGF0YS1hbmFseXRpY3MiLCJub25jZSI6IjY4ODFhNWM1LTNmZjMtNDZjZC1hNGYxLTkyYmY0NjEwOGM3YyIsInNlc3Npb25fc3RhdGUiOiI3YTQwODllMy03ZTlhLTQ0MzMtOWI5OC0zOWRkMDE3M2U3ZmYiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIk1FUy1Db0JyYUQiLCJhbGxfYW5hbHl0aWNzIiwidmFyaWFibGVzIiwidmlldy1hbmFseXRpY3MiLCJ1cGxvYWQtZGF0YSIsInF1ZXN0aW9ucyIsInZpZXctcWIiLCJtZXRhZGF0YS1hZG1pbiIsInZpZXctZXhwZXJ0LXN5c3RlbSIsInF1ZXN0aW9ubmFpcmVzIiwiZGVmYXVsdC1yb2xlcy1tZXNjb2JyYWQiLCJldS1zdXJ2ZXkiLCJkYXRhLW1hbmFnZXIiLCJ2aWV3LXJlc3VsdHMiLCJvZmZsaW5lX2FjY2VzcyIsIndvcmtzcGFjZXMiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImNhdGVnb3JpZXMiLCJtYXN0ZXItYWRtaW4iLCJ2aWV3LWNhdGVnb3JpZXMiLCJtZXRhZGF0YS1tYW5hZ2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiaG9tZS1hcHAiOnsicm9sZXMiOlsiTUVTLUNvQnJhRCJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsInNpZCI6IjdhNDA4OWUzLTdlOWEtNDQzMy05Yjk4LTM5ZGQwMTczZTdmZiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoiTWljaGFlbCBLb250b3VsaXMiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJta29udG91bGlzQGVwdS5udHVhLmdyIiwibG9jYWxlIjoiZW4iLCJnaXZlbl9uYW1lIjoiTWljaGFlbCIsImZhbWlseV9uYW1lIjoiS29udG91bGlzIiwiZW1haWwiOiJta29udG91bGlzQGVwdS5udHVhLmdyIn0.H6nwbIL7F4C-T6AILstsiAg0x0zr5YOkvwJg36KnVScXzA5lVlYV9lJWhjfsorR5WGxqCQ_oWYaz6emU7gK-_po8eDXznXmaUCtR_3OyvwQA8YuywV61FHaKHNQQ8aknaX4kSwAcpuBjpwRJMU8v_Wl_HvoNiMSKuoRl3fFKI3zoO3uxyJC9Dn2r6FPAvD8TW7_6meej3sijtSC3oQVGuTrQfgbUNenQ4xZNSa3hZmJfEuO4XKFU2OjCj6fFpDJ4hDAepw6K_t9ZXNPU1LoTOPR8m2X3HHYH2Rca8uAB0E_w0TydkTZau5kPx3AX3D8pdEfhr5Oa1Lko5nXqke5pSw")
         else:
             # print("NOT EXIST KEY")
             # print(navigation_item.metadata)
             create_local_step(workflow_id=navigation_item.workflow_id,
                               run_id=navigation_item.run_id, step_id=navigation_item.step_id, files_to_download=[],
-                              session_token=request.session.get("secret_key"))
+                              session_token=my_session_token)
+                              # session_token=request.session.get("secret_key"))
 
     # Add step and run id to the parameters
     url_to_redirect += "/?run_id=" + navigation_item.run_id + "&step_id=" + navigation_item.step_id + \
