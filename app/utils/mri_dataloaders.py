07ac327e98b22df1e7d8d472651035f25c7851ce
vlamprou <vlamprou@epu.ntua.gr>
2024-06-05 15:23:29 +0300

    moved transforms from the generator class into the dataloader methods

--- app/utils/mri_dataloaders.py
+++ app/utils/mri_dataloaders.py
@@ -1,7 +1,21 @@
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader, random_split
+from torchvision import transforms
 from app.utils.mri_generator import MRI_Generator
+
+class TransformedDataset(Dataset):
+    def __init__(self, dataset, transform):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        x, y = self.dataset[idx]
+        x = self.transform(x)
+        return x, y
 
 def train_eval_dataloaders(data_path,
                            csv_path,
@@ -9,10 +23,20 @@ def train_eval_dataloaders(data_path,
                            train_split=0.8):
     dataset = MRI_Generator(data_path, csv_path)
 
-    train_size = int(train_split * len(dataset))
-    val_size = len(dataset) - train_size
+    # Define the transformations
+    transform = transforms.Compose([
+        transforms.Lambda(lambda x: (x - x.min()) / (x.max() - x.min())),  # Normalize to [0, 1]
+        transforms.RandomHorizontalFlip(p=0.5),  # Apply random horizontal flipping with a probability of 0.5
+        transforms.RandomZoomOut(p=0.5)
+    ])
 
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    # Apply the transformations
+    transformed_dataset = TransformedDataset(dataset, transform)
+
+    train_size = int(train_split * len(transformed_dataset))
+    val_size = len(transformed_dataset) - train_size
+
+    train_dataset, val_dataset = random_split(transformed_dataset, [train_size, val_size])
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
@@ -27,7 +51,14 @@ def test_dataloader(data_path,
                     batch_size):
 
     dataset = MRI_Generator(data_path, csv_path)
-    test_dataloader = DataLoader(dataset,
+
+    transform = transforms.Compose([
+        transforms.Lambda(lambda x: (x - x.min()) / (x.max() - x.min())),  # Normalize to [0, 1]
+    ])
+
+    transformed_dataset = TransformedDataset(dataset, transform)
+
+    test_dataloader = DataLoader(transformed_dataset,
                                  batch_size=batch_size,
                                  shuffle=False)
 
