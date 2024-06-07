10ee8aa51316ae76ba3af6649c49a649d0ad63ba
theopnt <theopountridis@gmail.com>
2024-06-07 19:21:53 +0300

    Add proceed button to mri

--- app/utils/utils_general.py
+++ app/utils/utils_general.py
@@ -574,22 +574,21 @@ def csv_stats_to_trino(workflow_id: str,
 
         TRINO_SCHEME = "https"
         timezone = pytz.timezone("UTC")
-        #access_token = request.session.get("secret_key", None)
 
         ##TODO WHEN MIDDLEWARE WORKS
-        # access_token = request.session.get("secret_key", None)
-        # groups = request.session.get("groups", None)
-        # institution = "staging_area"
-        # if groups != None:
-        #     for group in groups:
-        #         if group in ["nia", "uu", "chs-rmc", "kcl", "sant-pau"]:
-        #             institution = group.replace("-", "_")
+        access_token = request.session.get("secret_key", None)
+        groups = request.session.get("groups", None)
+        institution = "staging_area"
+        if groups != None:
+            for group in groups:
+                if group in ["nia", "uu", "chs-rmc", "kcl", "sant-pau"]:
+                    institution = group.replace("-", "_")
 
         ##TODO TEMPORARY SOLUTION
-        text_file = open("token.txt", "r")
-        access_token = text_file.read()
-        text_file.close()
-        institution = "staging_area"
+        # text_file = open("token.txt", "r")
+        # access_token = text_file.read()
+        # text_file.close()
+        # institution = "staging_area"
 
 
         engine = create_engine(
@@ -640,7 +639,7 @@ def csv_stats_to_trino(workflow_id: str,
             'workspace_id': 'str'
         }
 
-        pd.DataFrame({col: pd.Series(dtype=dt) for col, dt in dtypes.items()}).to_sql(name='testtest999',
+        pd.DataFrame({col: pd.Series(dtype=dt) for col, dt in dtypes.items()}).to_sql(name='testtest9999',
                                                                                       schema=institution,
                                                                                       con=conn,
                                                                                       if_exists='append',
@@ -650,11 +649,11 @@ def csv_stats_to_trino(workflow_id: str,
 
         #Delete all old data with the same workflow_id, etc.
         conn.execute(f"\
-        DELETE FROM iceberg.{institution}.testtest999 WHERE source = '{source_file}'")
+        DELETE FROM iceberg.{institution}.testtest9999 WHERE source = '{source_file}'")
 
         df.to_csv("general_test.csv")
 
-        df.to_sql(name="testtest999", schema=institution, con=conn, if_exists='append',
+        df.to_sql(name="testtest9999", schema=institution, con=conn, if_exists='append',
                   index=False, method='multi')
 
         return 'Success'
